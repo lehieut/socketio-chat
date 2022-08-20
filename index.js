6284ef7de07e7265f973d965a87bffc4d4277fd9
Hieu <le.hieu.t@gmail.com>
2022-08-20 16:01:00 -0500

    updating port number to work on heroku

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(process.env.PORT || 3000, () => {
+  const portnum = process.env.PORT || 3000;
+  console.log('listening on *:' + portnum);
 });
